4941056427d8288b88add09d1f79edd1c30100d0
Heigen007 <67007974+Heigen007@users.noreply.github.com>
2021-08-11 19:42:14 +0600

    Update

--- wtl/src/App.vue
+++ wtl/src/App.vue
@@ -3,17 +3,16 @@
         <div class="MainBox">
       <div class="row">
         <span>Choose the period of time：</span>
-        <DatePicker v-model="time" type='datetime' format= "YYYY-MM-DD HH:mm" range></DatePicker>
+        <DatePicker v-model="time" type='date' format= "YYYY-MM-DD" range></DatePicker>
       </div>
       <hr style='width: 100%; box-shadow: 0 0 3px rgb(0,0,0)'>
       <div class="Week">
         <div v-for="(el,i) in WeekDays" :key = i class="row WeekDay MainBox">
           <span>{{el.name}}</span>
-          <DatePicker v-if='!el.IsTime' v-model="el.time" type='time' format= "HH:mm" range></DatePicker>
           <div>Holiday day <input type="checkbox" v-model='el.IsTime'></div>
           <div class='DayRests' v-if='!el.IsTime'>
-            <div class = 'Button' @click='el.rests.push([])' style='font-size: 1.3rem; display: flex; flex-direction: column; align-items: center'>Add rest</div>
-            <div v-if='el.rests.length > 0' class = 'Button Delete' @click='el.rests.pop()' style='font-size: 1.3rem; display: flex; flex-direction: column; align-items: center'>Delete rest</div>
+            <div class = 'Button' @click='el.rests.push([])' style='font-size: 1.3rem; display: flex; flex-direction: column; align-items: center'>Add working range</div>
+            <div v-if='el.rests.length > 0' class = 'Button Delete' @click='el.rests.pop()' style='font-size: 1.3rem; display: flex; flex-direction: column; align-items: center'>Delete working range</div>
             <div v-for='(rest, o) in el.rests' :key='o'>
               <DatePicker v-model="el.rests[o]" type='time' format= "HH:mm" range></DatePicker>
             </div>
@@ -30,18 +29,22 @@
 
       <hr style='width: 100%; box-shadow: 0 0 3px rgb(0,0,0)'>
       <div style='font-size:'>Extra days functional</div>
-      <div class = 'Button' @click='ExtraDays.push([])' style='font-size: 1.3rem; display: flex; flex-direction: column; align-items: center'>Add extra day</div>
+      <div class = 'Button' @click='ExtraDays.push({date: null, ranges: []})' style='font-size: 1.3rem; display: flex; flex-direction: column; align-items: center'>Add extra day</div>
       <div v-if='ExtraDays.length > 0' class = 'Button Delete' @click='ExtraDays.pop()' style='font-size: 1.3rem; display: flex; flex-direction: column; align-items: center'>Delete extra day</div>
       <div v-for='(rest, y) in ExtraDays' class='WeekDay' :key='y+"y"'>
-        <div v-if='ExtraDays[y][0]'>Day: {{ExtraDays[y][0].toLocaleString('en-US', options)}}</div>
+        <div v-if='ExtraDays[y].date'>Day: {{ExtraDays[y].date.toLocaleString('en-US', options)}}</div>
         <div v-else>Choose the day: </div>
-        <DatePicker v-model="ExtraDays[y][0]"></DatePicker>
+        <DatePicker v-model="ExtraDays[y].date"></DatePicker>
         <div>Choose the time range:</div>
-        <DatePicker v-model="ExtraDays[y][1]" type='time' format= "HH:mm" range></DatePicker>
+        <div class = 'Button' @click='ExtraDays[y].ranges.push([])' style='font-size: 1.3rem; display: flex; flex-direction: column; align-items: center'>Add time range</div>
+
+            <div v-for='(range, o) in ExtraDays[y].ranges' :key='o'>
+              <DatePicker v-model="ExtraDays[y].ranges[o]" type='time' format= "HH:mm" range></DatePicker>
+            </div>
       </div>
 
       <hr style='width: 100%; box-shadow: 0 0 3px rgb(0,0,0)'>
-      <div class="Button success" style='font-size: 1.3rem;' @click='CalcTime'>Compute the working time</div>
+      <div class="Button success" style='font-size: 1.3rem;' @click='computeTime'>Compute the working time</div>
     </div>
   </div>
 </template>
@@ -93,21 +96,135 @@ export default {
           ]
         },
       ],
+      segmentsCopy: [
+
+      ],
       validatyRange: [
         "2021-8-10",
         "2021-8-16"
       ],
-      options: {
+      functionOptions: {
         format: 'Hours', //Seconds | Minutes | Hours
-      }
+      },
+      time: null,
+      WeekDays: [
+        {
+          name: 'Sunday',
+          title: '',
+          rests: [],
+          workingHours: [],
+          IsTime: false
+        },
+        {
+          name: 'Monday',
+          title: '',
+          workingHours: [],
+          rests: [],
+          IsTime: false
+        },
+        {
+          name: 'Tuesday',
+          title: '',
+          workingHours: [],
+          rests: [],
+          IsTime: false
+        },
+        {
+          name: 'Wednesday',
+          title: '',
+          workingHours: [],
+          rests: [],
+          IsTime: false
+        },
+        {
+          name: 'Thursday',
+          title: '',
+          workingHours: [],
+          rests: [],
+          IsTime: false
+        },
+        {
+          name: 'Friday',
+          title: '',
+          workingHours: [],
+          rests: [],
+          IsTime: false
+        },
+        {
+          name: 'Saturday',
+          title: '',
+          workingHours: [],
+          rests: [],
+          IsTime: false
+        }
+      ],
+      InnerArray: [],
+      Holidays: [],
+      ExtraDays: [],
+      options: { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
     }
   },
   components: {
     DatePicker
   },
-  mounted() {
-    var answer = FindWorkingPeriod(this.validatyRange,this.segments,this.options)
-    console.log(answer);
+  methods: {
+    computeTime() {
+      this.segmentsCopy = []
+      this.validatyRange = [`${this.time[0].getFullYear()}-${this.time[0].getMonth() + 1}-${this.time[0].getDate()}`,`${this.time[1].getFullYear()}-${this.time[1].getMonth() + 1}-${this.time[1].getDate()}`]
+      var arr = [];
+      this.Holidays.forEach(el => {
+        if(el) arr.push(`${el.getFullYear()}-${el.getMonth() + 1}-${el.getDate()}`)
+      })
+      this.segmentsCopy.push({
+        name: 'Segment3Lvl',
+        description: '3 lvl segment for holidays',
+        status: true,
+        segmentLevel: 3,
+        segmentValidatyDays: arr
+      })
+
+      var segmentPeriods = {}
+      this.WeekDays.forEach(el => {
+        el.rests.forEach(rest => {
+          if(rest.length == 0) return
+          if(!segmentPeriods.hasOwnProperty(el.name)) segmentPeriods[el.name] = []
+          console.log(rest);
+          segmentPeriods[el.name].push([`${rest[0].getHours()}:${rest[0].getMinutes()}:${rest[0].getSeconds()}`,`${rest[1].getHours()}:${rest[1].getMinutes()}:${rest[1].getSeconds()}`])
+        })
+      })
+
+      this.segmentsCopy.push({
+        name: 'Segment1LvlWorkingTime',
+        description: '1 lvl segment for ordinary week days',
+        status: true,
+        validityStartDate: "2010-1-1",
+        validityEndDate: "2050-1-1",
+        segmentWorkingPeriods: segmentPeriods,
+        segmentLevel: 1
+      })
+
+      this.ExtraDays.forEach(el => {
+        if (el.date != null && el.ranges.length > 0) {
+          var ranges = []
+          el.ranges.forEach(range => {
+            if(range.length > 0) ranges.push([`${range[0].getHours()}:${range[0].getMinutes()}:${range[0].getSeconds()}`,`${range[1].getHours()}:${range[1].getMinutes()}:${range[1].getSeconds()}`])
+          })
+          this.segmentsCopy.push({
+            name: 'Segment2LvlWorkingTime',
+            description: '2 lvl segment for extra days',
+            status: true,
+            segmentWorkingPeriod: ranges,
+            segmentLevel: 2,
+            segmentValidatyDays: [
+              `${el.date.getFullYear()}-${el.date.getMonth() + 1}-${el.date.getDate()}`
+            ]
+          })
+        }
+      })
+
+      var answer = FindWorkingPeriod(this.validatyRange,this.segmentsCopy,this.functionOptions)
+      console.log(answer);
+    }
   }
 }
 </script>
